22cc36e7f4cab342305d0a7e2514cc057c33ef59
F. Chan <edu.fechan@gmail.com>
2020-03-27 14:18:45 -0700

    add more blurbs

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ let taglines = [
     "The ducks stay in Central Park, huddling together.",
     "Also check out the 404 page!",
     "Did you get the broom closet ending?",
-    "Scrambled eggs in a hot dog bun makes a cheap meal"
+    "Scrambled eggs in a hot dog bun makes a cheap meal.",
+    "Remember to wash your hands and cover your coughs!"
 ];
 let visits = parseInt(document.cookie.replace(/(?:(?:^|.*;\s*)visits\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
 if (isNaN(visits)){
